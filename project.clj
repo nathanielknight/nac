97bf2bfa81f9b271d120c0c28d820605e3bc18f8
Nat Egan-Pimblett <nathaniel.ep@gmail.com>
2014-09-11 14:42:07 -0700

    remove math dependency

--- project.clj
+++ project.clj
@@ -5,7 +5,6 @@
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/clojurescript "0.0-2311"]
                  [org.clojure/core.async "0.1.267.0-0d7780-alpha"]
-                 [org.clojure/math.combinatorics "0.0.8"]
                  [om "0.7.1"]]
 
   :plugins [[lein-cljsbuild "1.0.4-SNAPSHOT"]]
